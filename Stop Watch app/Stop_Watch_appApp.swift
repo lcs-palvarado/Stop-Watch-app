b9a14fd7c997b383431c97b704f1628db2d79cda
Pablo Alvarado <palvarado@lcs.on.ca>
2023-10-31 19:24:50 -0400

    Initial Commit

---
+++ Stop Watch app/Stop_Watch_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stop_Watch_appApp.swift
+//  Stop Watch app
+//
+//  Created by Pablo Alvarado on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Stop_Watch_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
